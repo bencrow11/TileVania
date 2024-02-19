10b6cd442ad8d69ef2de42ccb398573b82e3af59
Ben <bencrow11@gmail.com>
2024-02-19 18:56:21 +0000

    Added lives and score to UI

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
-    [SerializeField] AudioClip coinPickupSFX;
+    [SerializeField] AudioClip coinPickupSFX; // Audio to play when the player picks up a coin.
+    [SerializeField] int pointsPerPickup = 100; // Score to give player when they pick up a coin.
 
     void OnTriggerEnter2D(Collider2D other)
     {
         // If the thing that entered the trigger is a player:
         if (other.tag == "Player")
         {
+            // Adds the score to the player.
+            FindObjectOfType<GameSession>().AddToScore(pointsPerPickup);
+
             // Play the pickup audio.
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position);
 
--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] int playerLives = 3; // The amount of lives the player has.
+    [SerializeField] int score = 0; // The score the player has.
+    [SerializeField] TextMeshProUGUI livesText; // The text that shows the lives.
+    [SerializeField] TextMeshProUGUI scoreText; // Text that shows the score.
 
     void Awake()
     {
@@ -24,6 +28,12 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        livesText.text = playerLives.ToString(); // Updates UI to show lives.
+        scoreText.text = score.ToString(); // Updates UI to show score.
+    }
+
     public void ProcessPlayerDeath()
     {
         // If the player still has lives left, just remove one.
@@ -38,11 +48,18 @@ public class GameSession : MonoBehaviour
         }
     }
 
+    public void AddToScore(int pointsToAdd)
+    {
+        score += pointsToAdd; // Adds the points to the score.
+        scoreText.text = score.ToString(); // Updates UI.
+    }
+
     void TakeLife()
     {
         playerLives--; // Removes 1 life from playerLives.
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // Get the current scene index.
         SceneManager.LoadScene(currentSceneIndex); // Reload the current scene.
+        livesText.text = playerLives.ToString(); // Updates UI.
     }
 
     void ResetGameSession()
